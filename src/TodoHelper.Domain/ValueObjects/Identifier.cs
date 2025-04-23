9bea89465bd6cb14aee5a6e45be2ce975ff1d5b6
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-23 19:48:31 -0400

    override equality in value object

--- src/TodoHelper.Domain/ValueObjects/Identifier.cs
+++ src/TodoHelper.Domain/ValueObjects/Identifier.cs
@@ -8,4 +8,13 @@ internal sealed class Identifier<T>
     private Identifier() => Value = Guid.NewGuid();
 
     internal static Identifier<T> CreateNew() => new();
+
+    public override bool Equals(object? obj) => obj is Identifier<T> other && Value == other.Value;
+
+    public override int GetHashCode() => HashCode.Combine(Value);
+
+    public static bool operator ==(Identifier<T> left, Identifier<T> right) =>
+        left is null ? right is null : right is not null && left.Equals(right);
+
+    public static bool operator !=(Identifier<T> left, Identifier<T> right) => !(left == right);
 }
