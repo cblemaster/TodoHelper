ed62dfa0f714920ec452b45e9a701bac4856c993
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-23 19:47:45 -0400

    add entity relationship

--- src/TodoHelper.Domain/Entities/Category.cs
+++ src/TodoHelper.Domain/Entities/Category.cs
@@ -6,4 +6,5 @@ namespace TodoHelper.Domain.Entities;
 internal sealed class Category : Entity<Category>
 {
     internal override Identifier<Category> Id { get; }
+    internal IEnumerable<Todo> Todos { get; }
 }
--- src/TodoHelper.Domain/Entities/Todo.cs
+++ src/TodoHelper.Domain/Entities/Todo.cs
@@ -6,4 +6,6 @@ namespace TodoHelper.Domain.Entities;
 internal sealed class Todo : Entity<Todo>
 {
     internal override Identifier<Todo> Id { get; }
+    internal Category Category { get; }
+    internal Identifier<Category> CategoryId { get; }
 }
