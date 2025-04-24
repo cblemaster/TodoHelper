e295b06f80859d6a3af930f1996f849963724c6e
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-23 20:25:27 -0400

    add value object

---
+++ src/TodoHelper.Domain/ValueObjects/Name.cs
@@ -0,0 +1,14 @@
+﻿
+namespace TodoHelper.Domain.ValueObjects;
+
+internal sealed class Name
+{
+    private const int MAX_LENGTH = 40;
+
+    internal string Value { get; }
+
+    private Name(string value) => Value = value;
+
+    // TODO: Validation
+    internal static Name CreateNew(string value) => new(value);
+}
