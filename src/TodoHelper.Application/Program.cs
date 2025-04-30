752b47786e1267bbc0030a4cdbf740fe53c91aa7
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-29 21:18:24 -0400

    add services to web app builder

--- src/TodoHelper.Application/Program.cs
+++ src/TodoHelper.Application/Program.cs
@@ -1,6 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using Microsoft.EntityFrameworkCore;
+using TodoHelper.DataAccess;
+using TodoHelper.DataAccess.Repository;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TodosDbContext>(options => options.UseSqlServer("Server=.;Database=TodoHelper;Trusted_Connection=true;Trust Server Certificate=true"));
+builder.Services.AddScoped<ITodosRepository, TodosRepository>();
+
+WebApplication app = builder.Build();
+
+app.MapGet("/", () => "Welcome!");
 
 app.Run();
