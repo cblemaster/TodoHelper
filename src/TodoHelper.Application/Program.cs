044031f59c529741bd38616876427ea3b23cf369
Brian LeMaster <brian.lemaster@outlook.com>
2025-04-30 19:58:08 -0400

    register services

--- src/TodoHelper.Application/Program.cs
+++ src/TodoHelper.Application/Program.cs
@@ -1,5 +1,8 @@
 
 using Microsoft.EntityFrameworkCore;
+using TodoHelper.Application.Features.CreateCategory;
+using TodoHelper.Application.Features.CreateTodo;
+using TodoHelper.Application.Interfaces;
 using TodoHelper.DataAccess;
 using TodoHelper.DataAccess.Repository;
 
@@ -8,6 +11,8 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 string conn = builder.Configuration.GetConnectionString("todo-helper-conn") ?? "Error getting connection string.";
 builder.Services.AddDbContext<TodosDbContext>(options => options.UseSqlServer(conn));
 builder.Services.AddScoped<ITodosRepository, TodosRepository>();
+builder.Services.AddScoped<ICommandHandler<CreateCategoryCommand, CreateCategoryResponse>, CreateCategoryHandler>();
+builder.Services.AddScoped<ICommandHandler<CreateTodoCommand, CreateTodoResponse>, CreateTodoHandler>();
 
 WebApplication app = builder.Build();
 
