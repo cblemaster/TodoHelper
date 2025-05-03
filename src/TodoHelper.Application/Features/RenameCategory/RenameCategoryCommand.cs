a0e040aed1c22ba8ce30300430fdc05a44ea398f
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-03 16:16:17 -0400

    add rename category

---
+++ src/TodoHelper.Application/Features/RenameCategory/RenameCategoryCommand.cs
@@ -0,0 +1,10 @@
+﻿
+using TodoHelper.Application.Interfaces;
+
+namespace TodoHelper.Application.Features.RenameCategory;
+
+public sealed class RenameCategoryCommand(Guid categoryId, string name) : ICommand<RenameCategoryResponse>
+{
+    public Guid CategoryId { get; } = categoryId;
+    public string Name { get; } = name;
+}
---
+++ src/TodoHelper.Application/Features/RenameCategory/RenameCategoryHandler.cs
@@ -0,0 +1,26 @@
+﻿
+using TodoHelper.Application.Features.DeleteCategory;
+using TodoHelper.Application.Interfaces;
+using TodoHelper.DataAccess.Repository;
+using TodoHelper.Domain.Entities;
+using TodoHelper.Domain.Results;
+
+namespace TodoHelper.Application.Features.RenameCategory;
+
+internal sealed class RenameCategoryHandler(ITodosRepository repository) : ICommandHandler<RenameCategoryCommand, RenameCategoryResponse>
+{
+    private readonly ITodosRepository _repository = repository;
+
+    public Task<Result<RenameCategoryResponse>> HandleAsync(RenameCategoryCommand command, CancellationToken cancellationToken = default)
+    {
+        if (_repository.GetCategories().Single(c => c.Id.Value == command.CategoryId) is not Category category)
+        {
+            return Task.FromResult(Result<RenameCategoryResponse>.Failure($"Category with id {command.CategoryId} not found."));
+        }
+        else
+        {
+            _ = _repository.DeleteCategoryAsync(category);
+            return Task.FromResult(Result<RenameCategoryResponse>.Success(new RenameCategoryResponse(true)));
+        }
+    }
+}
