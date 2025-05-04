fe5ff7cc80321362a427753a474c88c1db5be9f7
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-04 19:09:05 -0400

    add migration

--- src/TodoHelper.DataAccess/Migrations/20250504230131_Add-Create-Update-Dates-To-Category.Designer.cs
+++ src/TodoHelper.DataAccess/Migrations/20250504230131_Add-Create-Update-Dates-To-Category.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TodoHelper.DataAccess;
 
@@ -11,9 +12,11 @@ using TodoHelper.DataAccess;
 namespace TodoHelper.DataAccess.Migrations
 {
     [DbContext(typeof(TodosDbContext))]
-    partial class TodosDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250504230131_Add-Create-Update-Dates-To-Category")]
+    partial class AddCreateUpdateDatesToCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/TodoHelper.DataAccess/Migrations/20250504230131_Add-Create-Update-Dates-To-Category.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoHelper.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreateUpdateDatesToCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreateDate",
+                table: "Category",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdateDate",
+                table: "Category",
+                type: "datetimeoffset",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateDate",
+                table: "Category");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateDate",
+                table: "Category");
+        }
+    }
+}
