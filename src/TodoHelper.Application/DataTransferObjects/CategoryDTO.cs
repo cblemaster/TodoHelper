092b2a4e70fb10b666fc05723e8feb5799a82915
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-04 19:57:37 -0400

    add dtos

---
+++ src/TodoHelper.Application/DataTransferObjects/CategoryDTO.cs
@@ -0,0 +1,4 @@
+﻿
+namespace TodoHelper.Application.DataTransferObjects;
+
+public sealed record CategoryDTO(Guid Id, string Name, int CountOfTodosNotComplete, DateTimeOffset CreateDate, DateTimeOffset? UpdateDate);
---
+++ src/TodoHelper.Application/DataTransferObjects/TodoDTO.cs
@@ -0,0 +1,5 @@
+﻿
+namespace TodoHelper.Application.DataTransferObjects;
+
+public sealed record TodoDTO(Guid Id, string CategoryName, Guid CategoryId, string Description, DateOnly? DueDate,
+    DateTimeOffset? CompleteDate, DateTimeOffset CreateDate, DateTimeOffset? UpdateDate, bool IsImportant);
