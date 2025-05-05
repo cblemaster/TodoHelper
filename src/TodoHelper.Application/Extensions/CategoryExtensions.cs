82dfd426bc5cc9110320638c757ebb1206798a6c
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-05 17:35:49 -0400

    add extensions for entitiy to dto mapping

---
+++ src/TodoHelper.Application/Extensions/CategoryExtensions.cs
@@ -0,0 +1,12 @@
+﻿
+using TodoHelper.Application.DataTransferObjects;
+using TodoHelper.Domain.Entities;
+
+namespace TodoHelper.Application.Extensions;
+
+internal static class CategoryExtensions
+{
+    internal static CategoryDTO MapToDTO(this Category category) =>
+        new(category.Id.Value, category.Name.Value, category.Todos.Count(t => !t.IsComplete),
+            category.CreateDate.Value, category.UpdateDate.Value);
+}
---
+++ src/TodoHelper.Application/Extensions/TodoExtensions.cs
@@ -0,0 +1,12 @@
+﻿
+using TodoHelper.Application.DataTransferObjects;
+using TodoHelper.Domain.Entities;
+
+namespace TodoHelper.Application.Extensions;
+
+internal static class TodoExtensions
+{
+    internal static TodoDTO MapToDTO(this Todo todo) =>
+        new(todo.Id.Value, todo.Category.Name.Value, todo.CategoryId.Value,
+            todo.Description.Value, todo.DueDate.Value, todo.CompleteDate.Value, todo.CreateDate.Value, todo.UpdateDate.Value, todo.Importance.IsImportant);
+}
