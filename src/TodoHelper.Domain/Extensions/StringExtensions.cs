504e21f01100dffa33b94fdfcfc83ce41a03b5ac
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-06 08:50:42 -0400

    merge identical value objects

---
+++ src/TodoHelper.Domain/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿
+namespace TodoHelper.Domain.Extensions;
+
+internal static class StringExtensions
+{
+    internal static (bool IsValid, string ValidationError) ValidateAttribute(this string s, string attributeName, int maxLength)
+    {
+        if (string.IsNullOrWhiteSpace(s))
+        {
+            return (false, DomainValidationErrors.IsNullEmptyOrWhitespaceErrorMessage(attributeName));
+        }
+        if (s.Length > maxLength)
+        {
+            return (false, DomainValidationErrors.MaxLengthExceededErrorMessage(attributeName, maxLength));
+        }
+        return (true, string.Empty);
+    }
+}
---
+++ src/TodoHelper.Domain/ValueObjects/Descriptor.cs
@@ -0,0 +1,20 @@
+﻿
+using TodoHelper.Domain.Extensions;
+using TodoHelper.Domain.Results;
+
+namespace TodoHelper.Domain.ValueObjects;
+
+public sealed class Descriptor
+{
+    public string Value { get; }
+
+    private Descriptor(string value) => Value = value;
+
+    public static Result<Descriptor> Create(string value, string attributeName, int maxLength)
+    {
+        (bool IsValid, string Error) = value.ValidateAttribute(attributeName, maxLength);
+        return IsValid
+            ? Result<Descriptor>.Success(new Descriptor(value))
+            : Result<Descriptor>.ValidationFailure(Error);
+    }
+}
