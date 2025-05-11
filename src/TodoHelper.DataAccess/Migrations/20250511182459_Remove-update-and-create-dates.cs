13e8fd9eaf31a790a7cb360b7256b658da1d21ed
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-11 14:28:53 -0400

    add migration

---
+++ src/TodoHelper.DataAccess/Migrations/20250511182459_Remove-update-and-create-dates.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoHelper.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Removeupdateandcreatedates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateDate",
+                table: "Todo");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateDate",
+                table: "Todo");
+
+            migrationBuilder.DropColumn(
+                name: "CreateDate",
+                table: "Category");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateDate",
+                table: "Category");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreateDate",
+                table: "Todo",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdateDate",
+                table: "Todo",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreateDate",
+                table: "Category",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdateDate",
+                table: "Category",
+                type: "datetimeoffset",
+                nullable: true);
+        }
+    }
+}
