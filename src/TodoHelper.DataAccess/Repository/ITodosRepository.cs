bd452702460fdc049af1f27ef1f4594d3644ff96
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-11 14:34:50 -0400

    rename repo methods

--- src/TodoHelper.DataAccess/Repository/ITodosRepository.cs
+++ src/TodoHelper.DataAccess/Repository/ITodosRepository.cs
@@ -5,9 +5,9 @@ namespace TodoHelper.DataAccess.Repository;
 
 public interface ITodosRepository<T> where T : Entity<T>
 {
-    Task Create(T entity, CancellationToken cancellationToken = default);
-    Task<T?> GetById(Guid id, CancellationToken cancellationToken = default);
-    Task<IEnumerable<T>> GetAll(CancellationToken cancellationToken = default);
-    Task Update(T entity, CancellationToken cancellationToken = default);
-    Task Delete(T entity, CancellationToken cancellationToken = default);
+    Task CreateAsync(T entity, CancellationToken cancellationToken = default);
+    Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
+    Task DeleteAsync(T entity, CancellationToken cancellationToken = default);
 }
--- src/TodoHelper.DataAccess/Repository/TodosRepository.cs
+++ src/TodoHelper.DataAccess/Repository/TodosRepository.cs
@@ -9,23 +9,23 @@ public sealed class TodosRepository<T>(TodosDbContext context) : ITodosRepositor
 {
     private readonly TodosDbContext _context = context;
 
-    public async Task Create(T entity, CancellationToken cancellationToken = default)
+    public async Task CreateAsync(T entity, CancellationToken cancellationToken = default)
     {
         _ = _context.Set<T>().Add(entity);
         _ = await _context.SaveChangesAsync(cancellationToken);
     }
 
-    public async Task<T?> GetById(Guid id, CancellationToken cancellationToken = default) => await _context.Set<T>().FindAsync(id, cancellationToken);
+    public async Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default) => await _context.Set<T>().FindAsync([id], cancellationToken: cancellationToken);
 
-    public async Task<IEnumerable<T>> GetAll(CancellationToken cancellationToken = default) => await _context.Set<T>().ToListAsync(cancellationToken);
+    public async Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default) => await _context.Set<T>().ToListAsync(cancellationToken);
 
-    public async Task Update(T entity, CancellationToken cancellationToken = default)
+    public async Task UpdateAsync(T entity, CancellationToken cancellationToken = default)
     {
         _ = _context.Set<T>().Update(entity);
         _ = await _context.SaveChangesAsync(cancellationToken);
     }
 
-    public async Task Delete(T entity, CancellationToken cancellationToken = default)
+    public async Task DeleteAsync(T entity, CancellationToken cancellationToken = default)
     {
         _ = _context.Set<T>().Remove(entity);
         _ = await _context.SaveChangesAsync(cancellationToken);
