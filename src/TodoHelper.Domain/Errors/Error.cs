5f7e08a6680f94dae55f661970ae3c905339f4ff
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-10 22:18:55 -0400

    add errors

---
+++ src/TodoHelper.Domain/Errors/Error.cs
@@ -0,0 +1,7 @@
+﻿
+namespace TodoHelper.Domain.Errors;
+
+public sealed record Error(string Code, string Description)
+{
+    public static readonly Error None = new("none", string.Empty);
+}
---
+++ src/TodoHelper.Domain/Errors/TodoErrors.cs
@@ -0,0 +1,14 @@
+﻿
+namespace TodoHelper.Domain.Errors;
+
+internal static class TodoErrors
+{
+    public static Error NotFound(Guid id) =>
+        new("Todos.NotFound", $"The todo with Id '{id}' was not found.");
+
+    public static Error DescriptionValueNotValid() =>
+        new("Todos.DescriptionValueNotValid", "Todo description is required and cannot consist of exclusively whitespace charaters.");
+
+    public static Error DescriptionLengthNotValid(int maxLength) =>
+        new("Todos.DescriptionLengthNotValid", $"Todo description cannot exceed {maxLength} characters.");
+}
