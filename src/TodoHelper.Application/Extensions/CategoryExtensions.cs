ecb62c66bd286aaed9e94f1f479d1b2325f9f952
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-12 09:59:54 -0400

    add mapping extension

---
+++ src/TodoHelper.Application/Extensions/CategoryExtensions.cs
@@ -0,0 +1,10 @@
+﻿
+using TodoHelper.Application.DataTransferObjects;
+
+namespace TodoHelper.Application.Extensions;
+
+internal static class CategoryExtensions
+{
+    internal static CategoryDTO MapToDTO(this Domain.Entities.Category category) =>
+        new(category.Id.Value, category.Name.Value, category.Todos.Count(t => !t.CompleteDate.HasValue));
+}
