d53551bd606c777f063086e1fe85552ae1c91526
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-12 08:27:13 -0400

    add error

--- src/TodoHelper.Domain/Errors/Error.cs
+++ src/TodoHelper.Domain/Errors/Error.cs
@@ -4,4 +4,5 @@ namespace TodoHelper.Domain.Errors;
 public sealed record Error(string Description)
 {
     public static readonly Error None = new(string.Empty);
+    public static readonly Error Unknown = new("An unknown error occurred.");
 }
