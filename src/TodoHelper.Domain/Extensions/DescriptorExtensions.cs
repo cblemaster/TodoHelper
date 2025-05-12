6fb2c99fb08501fd981e14dda82f233e2c13a633
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-11 20:45:07 -0400

    add extension method

---
+++ src/TodoHelper.Domain/Extensions/DescriptorExtensions.cs
@@ -0,0 +1,39 @@
+﻿
+using TodoHelper.Domain.Errors;
+using TodoHelper.Domain.Results;
+using TodoHelper.Domain.ValueObjects;
+
+namespace TodoHelper.Domain.Extensions;
+
+internal static class DescriptorExtensions
+{
+    internal static Result<Descriptor> Validate(this Descriptor descriptor)
+    {
+        string descriptorValue = descriptor.Value;
+        string attribute = descriptor.AttributeName;
+        int maxLength = descriptor.MaxLength;
+
+        if (string.IsNullOrEmpty(attribute))
+        {
+            attribute = "<field name>";
+        }
+        if (maxLength < 1)
+        {
+            maxLength = 1;
+        }
+                
+        string validationError = string.Empty;
+        if (string.IsNullOrWhiteSpace(descriptor.Value))
+        {
+            validationError += $"\n{attribute} is required and cannot consist exclusively of whitespace characters.";
+        }
+        if (descriptorValue.Length > maxLength)
+        {
+            validationError += $"\n{attribute} must be {maxLength} characters or fewer.";
+        }
+
+        return validationError != string.Empty
+            ? Result<Descriptor>.Failure(DescriptorErrors.NotValid(validationError))
+            : Result<Descriptor>.Success(descriptor);
+    }
+}
