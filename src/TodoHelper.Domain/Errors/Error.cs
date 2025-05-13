5ae21b605745526f03196fdaf15dc84a4264e67b
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-13 19:32:26 -0400

    add error

--- src/TodoHelper.Domain/Errors/Error.cs
+++ src/TodoHelper.Domain/Errors/Error.cs
@@ -5,4 +5,5 @@ public sealed record Error(string Description)
 {
     public static readonly Error None = new(string.Empty);
     public static readonly Error Unknown = new("An unknown error occurred.");
+    public static readonly Error NotFound = new("The requested resource cannot be found.");
 }
