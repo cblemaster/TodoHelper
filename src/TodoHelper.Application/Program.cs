dba940028c02775da9ae3745272aa9de34bce890
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-14 21:17:04 -0400

    refactor program.cs

--- src/TodoHelper.Application/Program.cs
+++ src/TodoHelper.Application/Program.cs
@@ -11,69 +11,87 @@ using CreateCategory = TodoHelper.Application.Features.Category.Create;
 using GetCategories = TodoHelper.Application.Features.Category.GetAll;
 using GetCategory = TodoHelper.Application.Features.Category.Get;
 using DeleteCategory = TodoHelper.Application.Features.Category.Delete;
+using UpdateCategory = TodoHelper.Application.Features.Category.Update;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 string conn = builder.Configuration.GetConnectionString("todo-helper-conn") ?? "Error getting connection string.";
 builder.Services.AddDbContext<TodosDbContext>(options => options.UseSqlServer(conn));
 
-builder.Services.AddScoped<ITodosRepository<Category>, TodosRepository<Category>>();
-builder.Services.AddScoped<ITodosRepository<Todo>, TodosRepository<Todo>>();
+builder.Services.AddScoped<IRepository<Category>, Repository<Category>>();
+builder.Services.AddScoped<IRepository<Todo>, Repository<Todo>>();
 builder.Services.AddScoped<CreateCategory.Handler>();
 builder.Services.AddScoped<GetCategories.Handler>();
 builder.Services.AddScoped<GetCategory.Handler>();
+builder.Services.AddScoped<UpdateCategory.Handler>();
 builder.Services.AddScoped<DeleteCategory.Handler>();
 
 WebApplication app = builder.Build();
 
 app.MapGet("/", () => "Welcome!");
 
+#region Category endpoints
 app.MapPost(pattern: "/category",
     handler: async Task<Results<BadRequest<string>, Created<CategoryDTO>, InternalServerError<string>>>
-    (ITodosRepository<Category> repository, CreateCategory.Command command, CreateCategory.Handler handler) =>
+    (IRepository<Category> repository, CreateCategory.Command command, CreateCategory.Handler handler) =>
     {
         Result<CreateCategory.Response> result = await handler.HandleAsync(command);
-        return result.IsFailure && result.Error is Error error
+        return result.IsFailure && result.Error is Error error && error.ErrorCode == ErrorCode.NotValid
             ? TypedResults.BadRequest(error.Description)
-            : result.IsSuccess && result.Value is not null and CreateCategory.Response response
+            : result.IsSuccess && result.Value is CreateCategory.Response response
                 ? TypedResults.Created("no uri for this resource", response.Category)
                 : TypedResults.InternalServerError(Error.Unknown.Description);
     });
 app.MapGet(pattern: "/category",
     handler: async Task<Results<InternalServerError<string>, Ok<IEnumerable<CategoryDTO>>>>
-    (ITodosRepository<Category> repository, GetCategories.Handler handler) =>
+    (IRepository<Category> repository, GetCategories.Handler handler) =>
     {
         GetCategories.Command command = new();
         Result<GetCategories.Response> result = await handler.HandleAsync(command);
-        return result.IsFailure && result.Error is Error error
-            ? TypedResults.InternalServerError(error.Description)
-            : result.IsSuccess && result.Value is not null and GetCategories.Response response
+        return result.IsFailure && result.Error is not null
+            ? TypedResults.InternalServerError(Error.Unknown.Description)
+            : result.IsSuccess && result.Value is GetCategories.Response response
                 ? TypedResults.Ok(response.Categories)
                 : TypedResults.InternalServerError(Error.Unknown.Description);
     });
 app.MapGet(pattern: "/category/{id:guid}",
     handler: async Task<Results<InternalServerError<string>, NotFound<string>, Ok<CategoryDTO>>>
-    (ITodosRepository<Category> repository, GetCategory.Handler handler, Guid id) =>
+    (IRepository<Category> repository, GetCategory.Handler handler, Guid id) =>
     {
         GetCategory.Command command = new(id);
         Result<GetCategory.Response> result = await handler.HandleAsync(command);
-        return result.IsFailure && result.Error is Error error
+        return result.IsFailure && result.Error is Error error && error.ErrorCode == ErrorCode.NotFound
             ? TypedResults.NotFound(error.Description)
             : result.IsSuccess && result.Value is not null and GetCategory.Response response
                 ? TypedResults.Ok(response.Category)
                 : TypedResults.InternalServerError(Error.Unknown.Description);
     });
-app.MapDelete(pattern: "/category/{id:guid}",
-    handler: async Task<Results<InternalServerError<string>, NotFound<string>, NoContent>>
-    (ITodosRepository<Category> repository, DeleteCategory.Handler handler, Guid id) =>
+app.MapPut(pattern: "/category/{id:guid}",
+    handler: async Task<Results<InternalServerError<string>, NotFound<string>, BadRequest<string>, NoContent>>
+    (IRepository<Category> repository, UpdateCategory.Command command, UpdateCategory.Handler handler, Guid id) =>
     {
-        DeleteCategory.Command command = new(id);
-        Result<DeleteCategory.Response> result = await handler.HandleAsync(command);
+        Result<UpdateCategory.Response> result = await handler.HandleAsync(command);
         return result.IsFailure && result.Error is Error error
-            ? TypedResults.NotFound(error.Description)
-            : result.IsSuccess && result.Value is not null and DeleteCategory.Response response
+            ? error.ErrorCode == ErrorCode.NotFound
+                ? TypedResults.NotFound(error.Description)
+                : error.ErrorCode == ErrorCode.NotValid
+                    ? TypedResults.BadRequest(error.Description)
+                    : TypedResults.InternalServerError(Error.Unknown.Description)
+            : result.IsSuccess && result.Value is UpdateCategory.Response response
                 ? TypedResults.NoContent()
                 : TypedResults.InternalServerError(Error.Unknown.Description);
     });
+app.MapDelete(pattern: "/category/{id:guid}",
+    handler: async Task<Results<InternalServerError<string>, NotFound<string>, NoContent>>
+    (IRepository<Category> repository, DeleteCategory.Command command, DeleteCategory.Handler handler, Guid id) =>
+    {
+        Result<DeleteCategory.Response> result = await handler.HandleAsync(command);
+        return result.IsFailure && result.Error is Error error && error.ErrorCode == ErrorCode.NotFound
+            ? TypedResults.NotFound(error.Description)
+            : result.IsSuccess && result.Value is DeleteCategory.Response response
+                ? TypedResults.NoContent()
+                : TypedResults.InternalServerError(Error.Unknown.Description);
+    });
+#endregion Category endpoints
 
 app.Run();
