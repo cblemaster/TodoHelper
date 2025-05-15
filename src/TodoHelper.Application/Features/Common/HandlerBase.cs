e2a68888c8c73fa70a3ad515a1913251bf046538
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-14 21:11:38 -0400

    fix handler base

--- src/TodoHelper.Application/Features/Common/HandlerBase.cs
+++ src/TodoHelper.Application/Features/Common/HandlerBase.cs
@@ -1,14 +1,15 @@
 ﻿
 using TodoHelper.Application.Interfaces;
 using TodoHelper.DataAccess.Repository;
+using TodoHelper.Domain.BaseClasses;
 using TodoHelper.Domain.Results;
 using _Category = TodoHelper.Domain.Entities.Category;
 
 namespace TodoHelper.Application.Features.Common;
 
-internal abstract class HandlerBase<TCommand, TResponse>(ITodosRepository<_Category> repository) : ICommandHandler<TCommand, TResponse>
+internal abstract class HandlerBase<T, TCommand, TResponse>(IRepository<T> repository) : ICommandHandler<TCommand, TResponse> where T :Entity<T>
 {
-    protected readonly ITodosRepository<_Category> _repository = repository;
+    protected readonly IRepository<T> _repository = repository;
 
     public abstract Task<Result<TResponse>> HandleAsync(TCommand command, CancellationToken cancellationToken = default);
 }
