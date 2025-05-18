ce419b66a7ef75fa25e812c33c2c785337ec5a01
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-18 14:06:23 -0400

    update access modifiers

--- src/TodoHelper.Domain/Primitives/Extensions/NullableDateTimeOffsetExtensions.cs
+++ src/TodoHelper.Domain/Primitives/Extensions/NullableDateTimeOffsetExtensions.cs
@@ -2,6 +2,7 @@
 using TodoHelper.Domain.ValueObjects;
 
 namespace TodoHelper.Domain.Primitives.Extensions;
+
 public static class NullableDateTimeOffsetExtensions
 {
     public static CompleteDate? MapToNullableCompleteDate(this DateTimeOffset? dateTimeOffset) =>
--- src/TodoHelper.Domain/Primitives/Extensions/StringExtensions.cs
+++ src/TodoHelper.Domain/Primitives/Extensions/StringExtensions.cs
@@ -1,9 +1,9 @@
 ﻿
 namespace TodoHelper.Domain.Primitives.Extensions;
 
-public static class StringExtensions
+internal static class StringExtensions
 {
-    public static bool IsValueValid(this string s) => !string.IsNullOrWhiteSpace(s);
-    public static bool IsLengthValid(this string s, uint maxLength) =>
+    internal static bool IsValueValid(this string s) => !string.IsNullOrWhiteSpace(s);
+    internal static bool IsLengthValid(this string s, uint maxLength) =>
         s.Length >= 1 && s.Length <= maxLength;  
 }
