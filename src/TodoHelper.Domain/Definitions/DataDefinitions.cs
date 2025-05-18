29eff4a5c3983bfe11ce06048c5337d7c35a3998
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-18 14:17:35 -0400

    code cleanup

--- src/TodoHelper.Domain/Definitions/DataDefinitions.cs
+++ src/TodoHelper.Domain/Definitions/DataDefinitions.cs
@@ -5,7 +5,10 @@ public static class DataDefinitions
 {
     public const int CATEGORY_NAME_MAX_LENGTH = 40;
     public const int TODO_DESCRIPTION_MAX_LENGTH = 255;
-
+    
     public const string CATEGORY_NAME_ATTRIBUTE = "Category name";
     public const string TODO_DESCRIPTION_ATTRIBUTE = "Todo description";
+
+    public const bool IS_CATEGORY_NAME_UNIQUE = true;
+    public const bool IS_TODO_DESCRIPTION_UNIQUE = false;
 }
