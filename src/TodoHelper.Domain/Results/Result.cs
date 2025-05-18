c69e62fdbbce63fc42e7bdfcc0489418f7b4d86c
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-18 14:10:50 -0400

    rename attribute

--- src/TodoHelper.Domain/Results/Result.cs
+++ src/TodoHelper.Domain/Results/Result.cs
@@ -7,16 +7,16 @@ public sealed class Result<T>
 {
     public bool IsSuccess { get; }
     public bool IsFailure => !IsSuccess;
-    public T? Value { get; }
+    public T? Payload { get; }
     public Error Error { get; }
 
-    private Result(bool isSuccess, T? value, Error error)
+    private Result(bool isSuccess, T? payload, Error error)
     {
         IsSuccess = isSuccess;
-        Value = value;
+        Payload = payload;
         Error = error;
     }
 
-    public static Result<T> Success(T value) => new(true, value, Error.None);
+    public static Result<T> Success(T payload) => new(true, payload, Error.None);
     public static Result<T> Failure(Error error) => new(false, default, error);
 }
