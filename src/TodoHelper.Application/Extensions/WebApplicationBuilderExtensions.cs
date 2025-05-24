60aa045a41733df09e691f439e8238b5166ba748
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-24 18:28:31 -0400

    refactor extensions

--- src/TodoHelper.Application/Extensions/WebApplicationBuilderExtensions.cs
+++ src/TodoHelper.Application/Extensions/WebApplicationBuilderExtensions.cs
@@ -11,6 +11,13 @@ using GetTodo = TodoHelper.Application.Features.Todo.Get;
 using GetTodos = TodoHelper.Application.Features.Todo.GetAll;
 using UpdateCategory = TodoHelper.Application.Features.Category.Update;
 using UpdateTodo = TodoHelper.Application.Features.Todo.Update;
+using UpdateTodoCompleteDate = TodoHelper.Application.Features.Todo.UpdateCompleteDate;
+using GetTodosByCategory = TodoHelper.Application.Features.Todo.GetByCategory;
+using GetTodosCompleted = TodoHelper.Application.Features.Todo.GetCompleted;
+using GetTodosDueToday = TodoHelper.Application.Features.Todo.GetDueToday;
+using GetTodosImportant = TodoHelper.Application.Features.Todo.GetImportant;
+using GetTodosOverdue = TodoHelper.Application.Features.Todo.GetOverdue;
+
 
 namespace TodoHelper.Application.Extensions;
 
@@ -19,18 +26,28 @@ internal static class WebApplicationBuilderExtensions
     internal static WebApplicationBuilder RegisterServices(this WebApplicationBuilder builder)
     {
         _ = builder.Services
+            
             .AddScoped<IRepository<Category>, TodoRepository<Category>>()
             .AddScoped<IRepository<Todo>, TodoRepository<Todo>>()
+
             .AddScoped<CreateCategory.Handler>()
+            .AddScoped<CreateTodo.Handler>()
             .AddScoped<GetCategories.Handler>()
             .AddScoped<GetCategory.Handler>()
-            .AddScoped<UpdateCategory.Handler>()
-            .AddScoped<DeleteCategory.Handler>()
-            .AddScoped<CreateTodo.Handler>()
             .AddScoped<GetTodos.Handler>()
             .AddScoped<GetTodo.Handler>()
+            .AddScoped<GetTodosByCategory.Handler>()
+            .AddScoped<GetTodosCompleted.Handler>()
+            .AddScoped<GetTodosDueToday.Handler>()
+            .AddScoped<GetTodosImportant.Handler>()
+            .AddScoped<GetTodosOverdue.Handler>()
+            .AddScoped<UpdateCategory.Handler>()
             .AddScoped<UpdateTodo.Handler>()
-            .AddScoped<DeleteTodo.Handler>();
+            .AddScoped<UpdateTodoCompleteDate.Handler>()
+            .AddScoped<DeleteCategory.Handler>()
+            .AddScoped<DeleteTodo.Handler>()
+            ;
+        
         return builder;
     }
 }
--- src/TodoHelper.Application/Extensions/WebApplicationExtensions.cs
+++ src/TodoHelper.Application/Extensions/WebApplicationExtensions.cs
@@ -8,7 +8,13 @@ using TodoHelper.Application.Features.Todo.Create;
 using TodoHelper.Application.Features.Todo.Delete;
 using TodoHelper.Application.Features.Todo.Get;
 using TodoHelper.Application.Features.Todo.GetAll;
+using TodoHelper.Application.Features.Todo.GetByCategory;
+using TodoHelper.Application.Features.Todo.GetCompleted;
+using TodoHelper.Application.Features.Todo.GetDueToday;
+using TodoHelper.Application.Features.Todo.GetImportant;
+using TodoHelper.Application.Features.Todo.GetOverdue;
 using TodoHelper.Application.Features.Todo.Update;
+using TodoHelper.Application.Features.Todo.UpdateCompleteDate;
 
 namespace TodoHelper.Application.Extensions;
 
@@ -18,14 +24,21 @@ internal static class WebApplicationExtensions
     {
         _ = app.MapCreateCategory()
             .MapCreateTodo()
-            .MapGetAllCategory()
-            .MapGetAllTodo()
+            .MapGetCategories()
+            .MapGetTodos()
             .MapGetCategory()
             .MapGetTodo()
+            .MapGetTodosByCategory()
+            .MapGetTodosCompleted()
+            .MapGetTodosDueToday()
+            .MapGetTodosImportant()
+            .MapGetTodosOverdue()
             .MapDeleteCategory()
             .MapDeleteTodo()
             .MapUpdateCategory()
-            .MapUpdateTodo();
+            .MapUpdateTodo()
+            .MapUpdateTodoCompleteDate()
+            ;
         return app;
     }
 }
