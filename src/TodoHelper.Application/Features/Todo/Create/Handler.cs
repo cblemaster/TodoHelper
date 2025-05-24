4e3a2328ae4addde01f457fe15fea4180eeac0b3
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-24 15:27:03 -0400

    fix handler

--- src/TodoHelper.Application/Features/Todo/Create/Handler.cs
+++ src/TodoHelper.Application/Features/Todo/Create/Handler.cs
@@ -11,19 +11,23 @@ using _Todo = TodoHelper.Domain.Entities.Todo;
 
 namespace TodoHelper.Application.Features.Todo.Create;
 
-internal sealed class Handler(IRepository<_Todo> repository) : HandlerBase<_Todo, Command, Response>(repository)
+internal sealed class Handler(IRepository<_Todo> todoRepository, IRepository<_Category> categoryRepository) : HandlerBase<_Todo, Command, Response>(todoRepository)
 {
+    private readonly IRepository<_Category> _categoryRepository = categoryRepository;
+
     public override async Task<Response> HandleAsync(Command command, CancellationToken cancellationToken = default)
     {
-        var repo = new TodoRepository<_Category>();
-
-        _Category? category = await repo.GetByIdAsync(command.CategoryId);  // TODO: what if this fails?
+        _Category? category = await _categoryRepository.GetByIdAsync(Identifier<_Category>.Create(command.CategoryId));
+        if (category is null)
+        {
+            return new Response(Result<TodoDTO>.Failure(Error.NotFound(nameof(_Category))));
+        }
 
         Result<_Todo> result = _Todo.CreateNew(category!, Identifier<_Category>.Create(command.CategoryId), command.Description, command.DueDate);
 
         if (result.IsFailure && result.Error is Error error)
         {
-            return new Response(Result<CategoryDTO>.Failure(Error.NotValid(error.Description)));
+            return new Response(Result<TodoDTO>.Failure(Error.NotValid(error.Description)));
         }
         else if (result.IsSuccess && result.Payload is _Todo todo)
         {
@@ -32,7 +36,7 @@ internal sealed class Handler(IRepository<_Todo> repository) : HandlerBase<_Todo
         }
         else
         {
-            return new Response(Result<CategoryDTO>.Failure(Error.Unknown));
+            return new Response(Result<TodoDTO>.Failure(Error.Unknown));
         }
     }
 }
