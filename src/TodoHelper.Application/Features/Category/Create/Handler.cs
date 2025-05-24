77719c0506ebdca8ae3f6087f1ac60034b792cf6
Brian LeMaster <brian.lemaster@outlook.com>
2025-05-24 13:19:45 -0400

    refactor creates

--- src/TodoHelper.Application/Features/Category/Create/Handler.cs
+++ src/TodoHelper.Application/Features/Category/Create/Handler.cs
@@ -1,4 +1,5 @@
 ﻿
+using TodoHelper.Application.DataTransferObjects;
 using TodoHelper.Application.Extensions;
 using TodoHelper.Application.Features.Common;
 using TodoHelper.DataAccess.Repository;
@@ -10,23 +11,26 @@ namespace TodoHelper.Application.Features.Category.Create;
 
 internal sealed class Handler(IRepository<_Category> repository) : HandlerBase<_Category, Command, Response>(repository)
 {
-    public override async Task<Result<Response>> HandleAsync(Command command, CancellationToken cancellationToken = default)
+    public override async Task<Response> HandleAsync(Command command, CancellationToken cancellationToken = default)
     {
         Result<_Category> result = _Category.CreateNew(command.Name);
 
         if (result.IsFailure && result.Error is Error error)
         {
-            return Result<Response>.Failure(Error.NotValid(error.Description));
+            return new Response(Result<CategoryDTO>.Failure(Error.NotValid(error.Description)));
+        }
+        else if ((await _repository.GetAllAsync()).Select(c => c.Name.Value).Contains(command.Name))
+        {
+            return new Response(Result<CategoryDTO>.Failure(Error.DomainRuleViolation($"Category with name {command.Name} already exists.")));
         }
         else if (result.IsSuccess && result.Payload is _Category category)
         {
             category = await _repository.CreateAsync(category);
-            Response response = new(category.MapToDTO());
-            return Result<Response>.Success(response);
+            return new Response(Result<CategoryDTO>.Success(category.MapToDTO()));
         }
         else
         {
-            return Result<Response>.Failure(Error.Unknown);
+            return new Response(Result<CategoryDTO>.Failure(Error.Unknown));
         }
     }
 }
--- src/TodoHelper.Application/Features/Todo/Create/Handler.cs
+++ src/TodoHelper.Application/Features/Todo/Create/Handler.cs
@@ -1,35 +1,38 @@
 ﻿
+using TodoHelper.Application.DataTransferObjects;
 using TodoHelper.Application.Extensions;
 using TodoHelper.Application.Features.Common;
 using TodoHelper.DataAccess.Repository;
+using TodoHelper.Domain.BaseClasses;
 using TodoHelper.Domain.Errors;
 using TodoHelper.Domain.Results;
-using _Todo = TodoHelper.Domain.Entities.Todo;
 using _Category = TodoHelper.Domain.Entities.Category;
-using TodoHelper.Domain.BaseClasses;
+using _Todo = TodoHelper.Domain.Entities.Todo;
+
 namespace TodoHelper.Application.Features.Todo.Create;
 
 internal sealed class Handler(IRepository<_Todo> repository) : HandlerBase<_Todo, Command, Response>(repository)
 {
-    public override async Task<Result<Response>> HandleAsync(Command command, CancellationToken cancellationToken = default)
+    public override async Task<Response> HandleAsync(Command command, CancellationToken cancellationToken = default)
     {
-        _Category? category = _Category.CreateNew("").Payload;
+        var repo = new TodoRepository<_Category>();
+
+        _Category? category = await repo.GetByIdAsync(command.CategoryId);  // TODO: what if this fails?
 
         Result<_Todo> result = _Todo.CreateNew(category!, Identifier<_Category>.Create(command.CategoryId), command.Description, command.DueDate);
 
         if (result.IsFailure && result.Error is Error error)
         {
-            return Result<Response>.Failure(Error.NotValid(error.Description));
+            return new Response(Result<CategoryDTO>.Failure(Error.NotValid(error.Description)));
         }
         else if (result.IsSuccess && result.Payload is _Todo todo)
         {
             todo = await _repository.CreateAsync(todo);
-            Response response = new(todo.MapToDTO());
-            return Result<Response>.Success(response);
+            return new Response(Result<TodoDTO>.Success(todo.MapToDTO()));
         }
         else
         {
-            return Result<Response>.Failure(Error.Unknown);
+            return new Response(Result<CategoryDTO>.Failure(Error.Unknown));
         }
     }
 }
